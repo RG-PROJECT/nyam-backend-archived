bd10590eda5d499b92ad27ae18dc896a209538a8
godori <gej.okpo@gmail.com>
2018-10-23 21:50:26 +0900

    move mongo atlas address to env file, add README file

---
+++ README.md
@@ -0,0 +1,9 @@
+# nyam-backend-memo
+
+### npm install list
+
+- express
+- nodemon (Auto update)
+- morgan (Displays method called on console)
+- body-parser
+- mongoose
--- app.js
+++ app.js
@@ -1,56 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
 const connect = () => {
-    console.log('connect success!')
-    const uri = 'mongodb://godori:'
-        + process.env.MONGO_ATLAS_PW
-        + '@nyam-shard-00-00-6akso.mongodb.net:27017,nyam-shard-00-01-6akso.mongodb.net:27017,nyam-shard-00-02-6akso.mongodb.net:27017/test?ssl=true&replicaSet=nyam-shard-0&authSource=admin&retryWrites=true'
-    return mongoose.connect(uri,{ useNewUrlParser: true});
+  console.log("connect success!");
+  const uri =
+    "mongodb://godori:" +
+    process.env.MONGO_ATLAS_PW +
+    process.env.MONGO_ATLAS_ADDR;
+  return mongoose.connect(
+    uri,
+    { useNewUrlParser: true }
+  );
 };
 
 connect();
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin","*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-        );
-    if(req.method === 'OPTIONS') {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
-        return res.status(200).json({});
-    }
-    next();
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
-app.use((req,res,next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error); // forward error request
-})
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error); // forward error request
+});
 
-// if database fails, handle here
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-})
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
