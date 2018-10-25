30232dc4dc9881b05b0d3f7c0e91b7a041d7c7f5
godori <gej.okpo@gmail.com>
2018-10-25 22:50:39 +0900

    Modify card model and add route

---
+++ api/models/cards.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const cardSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  status: { type: String, default: "OK" },
+  title: { type: String, required: true },
+  // ㄴ 최대한 자동으로 제목 생성 hh시 mm분에 저장한 author의 기록
+  author: { type: String, required: true },
+  regdate: { type: Date, default: Date.now },
+  img: { type: String },
+  text: { type: String }
+  //   contents: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "Contents",
+  //     required: true
+  //   }
+  //   reactionList: [Schema.Types.ObjectId],
+  //   commentList: [Schema.Types.ObjectId]
+});
+
+module.exports = mongoose.model("Card", cardSchema);
---
+++ api/routes/cards.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Card = require("../models/cards");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  Card.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res, next) => {
+  // min 200 ~ max 300
+  const randNum = Math.floor(Math.random() * (300 - 200 + 1)) + 200;
+  const card = new Card({
+    _id: new mongoose.Types.ObjectId(),
+    title: req.body.title,
+    author: req.body.author,
+    img: "https://picsum.photos/" + randNum,
+    text: "Hello Nyam! 냠 🐿"
+  });
+
+  card
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "SUCCESS Creating Card",
+        createCard: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+module.exports = router;
