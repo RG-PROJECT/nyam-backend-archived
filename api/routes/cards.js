dc211ecd0b143a36dd44e706a72875d2f293c547
Chanwoo Noh <rajephon@gmail.com>
2019-01-26 01:54:42 +0900

    페이징

--- api/routes/cards.js
+++ api/routes/cards.js
@@ -4,44 +4,60 @@ const Card = require("../models/cards");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  Card.find().sort({regdate: -1})
-    .exec()
-    .then(docs => {
-      console.log(docs);
-      res.status(200).json(docs);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    let page = 1;
+    let limit = 10;
+    if (typeof req.query['page'] !== 'undefined' && Number.isInteger(Number(req.query['page']))) {
+        const v = Number(req.query['page']);
+        if (v > 0)
+            page = v;
+    }
+    if (typeof req.query['limit'] !== 'undefined' && Number.isInteger(Number(req.query['limit']))) {
+        const v = Number(req.query['limit']);
+        if (0 < v && v <= 20)
+            limit = v;
+    }
+    console.log(`page:${page}, limit:${limit}`);
+    Card.find()
+        .sort({regdate: -1})
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post("/", (req, res, next) => {
-  // min 200 ~ max 300
-  const randNum = Math.floor(Math.random() * (300 - 200 + 1)) + 200;
-  const card = new Card({
-    _id: new mongoose.Types.ObjectId(),
-    title: req.body.title,
-    author: req.body.author,
-    img: "https://picsum.photos/" + randNum,
-    text: "Hello Nyam! 냠 🐿"
-  });
-
-  card
-    .save()
-    .then(result => {
-      console.log(result);
-      res.status(201).json({
-        message: "SUCCESS Creating Card",
-        createCard: result
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
+    // min 200 ~ max 300
+    const randNum = Math.floor(Math.random() * (300 - 200 + 1)) + 200;
+    const card = new Card({
+        _id: new mongoose.Types.ObjectId(),
+        title: req.body.title,
+        author: req.body.author,
+        img: "https://picsum.photos/" + randNum,
+        text: "Hello Nyam! 냠 🐿"
     });
+
+    card
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: "SUCCESS Creating Card",
+                createCard: result
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
